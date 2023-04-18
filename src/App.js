db5bf71e5a7139953d434dd9659373c8d137878c
Flavia-FT <flaviatelcian@yahoo.com>
2023-04-19 03:07:00 +0900

    d

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
+import Button2 from './Button2';
 
 function App() {
   return (
@@ -9,14 +11,8 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <Button/>
+       <Button2/>
       </header>
     </div>
   );
---
+++ src/Button2.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button2=() => {
+    return(
+        <button>Button2</button>
+    )
+}
+
+export default Button2;
