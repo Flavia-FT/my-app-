468fa09019e718568c358d49c1b6e3438db30494
Flavia-FT <flaviatelcian@yahoo.com>
2023-04-19 04:14:34 +0900

    b

--- src/Button2.js
+++ src/Button2.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Button2=() => {
-    return(
-        <button>Button2</button>
+const Button2 = ({message = "Click Me"}) =>{
+    return (
+        <button>{message}</button>
     )
-}
+    }
+// const Button2=() => {
+//     return(
+//         <button>Button2</button>
+//     )
+// }
 
 export default Button2;
