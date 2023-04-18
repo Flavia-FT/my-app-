85bc9b93015cd1e1d35163be56cdad3c8bec8d32
Flavia-FT <flaviatelcian@yahoo.com>
2023-04-19 03:19:45 +0900

    b

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Button from './Button';
 import Button2 from './Button2';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+      <Welcome name="Flavia"/>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
